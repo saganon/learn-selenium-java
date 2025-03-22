024e4bf52f565f8dd8452dfb7ef6a1f1cca1735c
saganon <shanonathooya@gmail.com>
2025-03-22 11:54:12 +0700

    implement save screenshot

--- src/main/java/setup/Main.java
+++ src/main/java/setup/Main.java
@@ -3,10 +3,16 @@ package setup;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.bidi.browsingcontext.BrowsingContext;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebElement;
 import org.openqa.selenium.support.ui.Select;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Base64;
 import java.util.List;
 
 import static io.github.bonigarcia.wdm.WebDriverManager.chromedriver;
@@ -18,14 +24,21 @@ public class Main {
         chromedriver().setup();
         var options = new ChromeOptions();
         options.addArguments("--no-sandbox");
+        options.enableBiDi();
         driver = new ChromeDriver(options);
     }
 
     public static void main(String[] args) {
         driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+        var browsingContext = new BrowsingContext(driver, driver.getWindowHandle());
+
+        String fullScreenshot = browsingContext.captureScreenshot();
+        saveScreenShot(fullScreenshot, "full_screenshot.png");
 
         WebElement textInput = driver.findElement(By.id("my-text-id"));
         textInput.sendKeys("learn selenium");
+        String elementId = ((RemoteWebElement) textInput).getId();
+        saveScreenShot(browsingContext.captureElementScreenshot(elementId), "text_input.png");
 
         WebElement passwordInput = driver.findElement(By.name("my-password"));
         passwordInput.sendKeys("1234");
@@ -58,7 +71,7 @@ public class Main {
         dropdown.selectByIndex(1);
          */
 
-        String filePath = "/Users/nb-dk-0288/Documents/personal-code/learn-selenium-java/resources/file.txt"; // absolute path
+        String filePath = "/resources/file.txt"; // absolute path
         WebElement upload = driver.findElement(By.name("my-file"));
         upload.sendKeys(filePath);
 
@@ -72,6 +85,22 @@ public class Main {
         returnToIndex.click();
         */
 
+        String title = driver.getTitle();
+        String url = driver.getCurrentUrl();
+
+        System.out.println(title);
+        System.out.println(url);
+
         driver.quit();
+    }
+
+    private static void saveScreenShot(String screenshot, String fileName) {
+        var decodedScreenshot = Base64.getDecoder().decode(screenshot);
+        try {
+            String path = "/learn-selenium-java/screenshots/";
+            Files.write(Paths.get(path + fileName), decodedScreenshot);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
